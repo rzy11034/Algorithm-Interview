a9ab9199b2972c88db1da0c3acc4ab626c7410e5
rzy11034@163.com <rzy11034@163.com>
2020-10-23 17:21:08 +0800

    2020-10-23 17:21

---
+++ Typhon/Source/Chapter05-About-Linked-List/06-Remove-Nth-Node-From-End-of-List/Chapter05._06_Solution1.pas
@@ -0,0 +1,73 @@
+﻿unit Chapter05._06_Solution1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  Chapter05.ListNode;
+
+// 19. Remove Nth Node From End of List
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+//
+// 使用双指针, 对链表只遍历了一遍
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+type
+  TSolution = class(TObject)
+  public
+    function removeNthFromEnd(head: TListNode; n: integer): TListNode;
+  end;
+
+procedure Main;
+
+implementation
+
+procedure Main;
+var
+  head: TListNode;
+begin
+  head := TListNode.Create([1, 2, 3, 4, 5]);
+
+  with TSolution.Create do
+  begin
+    head := removeNthFromEnd(head, 1);
+    WriteLn(head.ToString);
+    Free;
+  end;
+
+  head.CLearAndFree;
+end;
+
+{ TSolution }
+
+function TSolution.removeNthFromEnd(head: TListNode; n: integer): TListNode;
+var
+  l, r, dummyHead, del: TListNode;
+  i: integer;
+begin
+  dummyHead := TListNode.Create(0);
+  dummyHead.Next := head;
+  l := dummyHead;
+  r := dummyHead;
+
+  for i := 0 to n do
+    r := r.Next;
+
+  while r <> nil do
+  begin
+    l := l.Next;
+    r := r.Next;
+  end;
+
+  del := l.Next;
+  l.Next := del.Next;
+  FreeAndNil(del);
+
+  Result := dummyHead.Next;
+  dummyHead.Free;
+end;
+
+end.
---
+++ Typhon/Source/Chapter06-Stack-and-Queue/01-Valid-Parentheses/Chapter06._01_Solution1.pas
@@ -0,0 +1,77 @@
+﻿unit Chapter06._01_Solution1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  DeepStar.Utils,
+  DeepStar.Utils.UString;
+
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/description/
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+type
+  TSolution = class(TObject)
+  public
+    function isValid(s: UString): boolean;
+  end;
+
+procedure Main;
+
+implementation
+
+procedure Main;
+begin
+  with TSolution.Create do
+  begin
+    WriteLn(isValid('()'));
+    WriteLn(isValid('()[]{}'));
+    WriteLn(isValid('(]'));
+    WriteLn(isValid('([)]'));
+    Free;
+  end;
+end;
+
+{ TSolution }
+
+function TSolution.isValid(s: UString): boolean;
+var
+  stack: TStack_chr;
+  i: integer;
+  match, c: UChar;
+begin
+  stack := TStack_chr.Create;
+  try
+    for i := 0 to Length(s) - 1 do
+    begin
+      if (s.Chars[i] = '(') or (s.Chars[i] = '[') or (s.Chars[i] = '{') then
+        stack.Push(s.Chars[i])
+      else
+      begin
+        if stack.IsEmpty then Exit(false);
+
+        if s.Chars[i] = ')' then
+          match := '('
+        else if s.Chars[i] = ']' then
+          match := '['
+        else
+          match := '{';
+
+
+        c := stack.Pop;
+
+        if c <> match then Exit(false);
+      end;
+    end;
+
+    Result := stack.IsEmpty;
+  finally
+    stack.Free;
+  end;
+end;
+
+end.
