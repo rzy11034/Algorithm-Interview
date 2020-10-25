b23d0cbf3cae2ef11de078773902e8afd19f9813
rzy11034 <rzy11034@163.com>
2020-10-26 00:53:31 +0800

    2020-10-26 00:53

---
+++ Typhon/Source/Chapter06-Stack-and-Queue/06-Priority-Queue/Chapter06._06_Solution1.pas
@@ -0,0 +1,64 @@
+﻿unit Chapter06._06_Solution1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils, DeepStar.DSA.Interfaces,
+  DeepStar.DSA.Tree.PriorityQueue;
+
+procedure Main;
+
+implementation
+
+type
+  TImpl_int = specialize TImpl<integer>;
+  TPQ_int = specialize TPriorityQueue<integer>;
+
+function bool(constref Left, Right: integer): integer;
+begin
+  Result := Left mod 10 - Right mod 10;
+end;
+
+procedure Main;
+var
+  pq1, pq2: TPQ_int;
+  num, i: integer;
+begin
+  Randomize;
+
+  pq1 := TPQ_int.Create;
+  for i := 0 to 9 do
+  begin
+    num := Random(100);
+    pq1.EnQueue(num);
+    WriteLn('insert ', num, ' in priority queue.');
+  end;
+
+  while not pq1.IsEmpty do
+  begin
+    Write(pq1.DeQueue, ' ');
+  end;
+  WriteLn;
+  pq1.Free;
+
+  // 使用TComparisonFuncs的priority queue, 底层是最小堆
+  pq2 := TPQ_int.Create(TImpl_int.TCmp.Construct(@bool));
+  for i := 0 to 9 do
+  begin
+    num := Random(100);
+    pq2.EnQueue(num);
+    WriteLn('insert ', num, ' in priority queue.');
+  end;
+
+  while not pq2.IsEmpty do
+  begin
+    Write(pq2.DeQueue, ' ');
+  end;
+  WriteLn;
+  pq2.Free;
+end;
+
+end.
---
+++ Typhon/Source/Chapter06-Stack-and-Queue/07-Top-K-Frequent-Elements/Chapter06._07_Solution1.pas
@@ -0,0 +1,115 @@
+﻿unit Chapter06._07_Solution1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  DeepStar.DSA.Interfaces,
+  DeepStar.Utils,
+  DeepStar.DSA.Tree.PriorityQueue;
+
+// 347. Top K Frequent Elements
+// https://leetcode.com/problems/top-k-frequent-elements/description/
+// 时间复杂度: O(nlogk)
+// 空间复杂度: O(n + k)
+type
+  TSolution = class(TObject)
+  public
+    function topKFrequent(a: TArr_int; k: integer): TList_int;
+  end;
+
+procedure Main;
+
+implementation
+
+procedure Main;
+var
+  a: TArr_int;
+  k: integer;
+  l: TList_int;
+begin
+  with TSolution.Create do
+  begin
+    a := [1, 1, 1, 2, 2, 3];
+    k := 2;
+    l := topKFrequent(a, k);
+    TArrayUtils_int.Print(l.ToArray);
+    l.Free;
+
+    a := [1];
+    k := 1;
+    l := topKFrequent(a, k);
+    TArrayUtils_int.Print(l.ToArray);
+    l.Free;
+
+    Free;
+  end;
+end;
+
+{ TSolution }
+
+function TSolution.topKFrequent(a: TArr_int; k: integer): TList_int;
+type
+  TPair = TMap_int_int.TPair;
+  TImpl_pair = specialize TImpl<TPair>;
+  TPQ_pair = specialize TPriorityQueue<TPair>;
+
+  function cmp(constref a, b: TMap_int_int.TPair): integer;
+  begin
+    Result := a.Value - b.Value;
+  end;
+
+var
+  map: TMap_int_int;
+  i: integer;
+  pq: TPQ_pair;
+  e: TPair;
+  res: TList_int;
+begin
+  Assert(k > 0);
+
+  map := TMap_int_int.Create;
+  try
+    for i := 0 to High(a) do
+    begin
+      if not map.ContainsKey(a[i]) then
+        map.Add(a[i], 1)
+      else
+        map.Add(a[i], map.GetItem(a[i]) + 1);
+    end;
+
+    pq := TPQ_pair.Create(TImpl_pair.TCmp.Construct(TImpl_pair.TComparisonFuncs(@cmp)));
+    try
+      for e in map.Pairs do
+      begin
+        if pq.Count >= k then
+        begin
+          if e.Value > pq.Peek.Value then
+            pq.DeQueue;
+        end
+        else
+          pq.EnQueue(TPair.Create(e.Key, e.Value));
+      end;
+
+      res := TList_int.Create;
+      while not pq.IsEmpty do
+      begin
+        e := pq.DeQueue;
+
+        res.AddFirst(e.Key);
+      end;
+
+      Result := res;
+
+    finally
+      pq.Free;
+    end;
+  finally
+    map.Free;
+  end;
+end;
+
+end.
